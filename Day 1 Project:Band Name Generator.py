fc211e229979c2098f9a5ffd63157b6a9944e3c1
Anchita Marodia <anchitamarodia07@gmail.com>
2025-03-09 23:11:32 +0530

    .

--- Day 1 Project:Band Name Generator.py
+++ Day 1 Project:Band Name Generator.py
@@ -9,4 +9,4 @@ print(pet)
 #4. Combine the name of their city and pet and show them their band name.
 print('your band name is '+ city + pet)
 #5. Make sure the input cursor shows on a new line:
-
+ 
